8ae0609b58681e24e730e4bb353eb0a3d22b09ff
Lucy <sc14lif@leeds.ac.uk>
2014-11-25 19:28:46 +0000

    Added initial files

---
+++ src/comp2541/coursework/cwk2/EventTest.java
@@ -0,0 +1,8 @@
+package comp2541.coursework.cwk2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EventTest
+{
+}
---
+++ src/comp2541/coursework/cwk2/Venue.java
@@ -0,0 +1,5 @@
+package comp2541.coursework.cwk2;
+
+public class Venue
+{
+}
