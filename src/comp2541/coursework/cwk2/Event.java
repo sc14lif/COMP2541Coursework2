b1e94bb3390151b5b1d30a2b65eba03eb3983070
Lucy <sc14lif@leeds.ac.uk>
2014-11-25 19:57:41 +0000

    Added fields and a constructor to Event class

--- src/comp2541/coursework/cwk2/Event.java
+++ src/comp2541/coursework/cwk2/Event.java
@@ -1,5 +1,22 @@
 package comp2541.coursework.cwk2;
+import java.util.List;
+import org.joda.money.Money;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 
 public class Event
 {
+	private List<String> artists;
+	private LocalDate date;
+	private LocalTime doors;
+	private Money ticketPrice;
+	private int ticketsSold;
+	
+	public Event(List<String> a, String date, String doors, String price, int sold){
+		this.artists = a;
+		this.date = LocalDate.parse(date);
+		this.doors = LocalTime.parse(doors);
+		this.ticketPrice = Money.parse(price);
+		this.ticketsSold = sold;
+	}
 }
